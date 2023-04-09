ee7fbe8ff560123d9c3da625eeff1c663caff92b
kellhusCorp <alkan_7@mail.ru>
2023-04-09 22:54:03 +0400

    4. Добавил VM.

---
+++ src/MyBlogOnCore/Models/UsersViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MyBlogOnCore.Domain;
+using MyBlogOnCore.Infrastructure.Paging;
+
+namespace MyBlogOnCore.Models;
+
+public class UsersViewModel
+{
+    public string? SearchTerm { get; set; }
+    
+    public PagedResult<User>? Users { get; set; }
+}
